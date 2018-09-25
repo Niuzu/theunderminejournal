b7c9fc692fe8918ad92bd5bc7ec735f71a67bdf8
Niuzu <markusherzberg94@gmail.com>
2018-09-25 04:33:39 +0200

    added a get_item_name function to getjson.py

--- getjson.py
+++ getjson.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_item_json(item_id, realm_index) -> dict:
+def __get_item_json__(item_id, realm_index) -> dict:
     """
     Returns auctionhouse information from 'https://theunderminejournal.com/'
 
@@ -19,10 +19,32 @@ def get_item_json(item_id, realm_index) -> dict:
 
     # construct the url string
     url_string = "https://theunderminejournal.com/api/item.php?house=" + \
-        realm_index + "&item=" + item_id
+        str(realm_index) + "&item=" + str(item_id)
 
     r = requests.get(url_string)  # get the json from underminejournal
 
     item_dict = r.json()  # construct dictionary
 
     return item_dict
+
+
+def get_item_name(item_id, realm_index) -> str:
+    """
+    Returns name from underminejournal json
+
+    Args:
+        item_id (int): Defines the item with an id. Id's can be found at: 'https://www.wowhead.com/items'
+        realm_index (int): Defines the realm with an id. List of Id's couln't be found. Thrall-EU = 238 
+
+    Returns:
+        str: Returns a string that defines the name of the item
+        None: Returns None if no items or data was found
+
+    Raises:
+        None
+    """
+
+    #  get item ison and direct to name
+    item_name = __get_item_json__(item_id, realm_index)["stats"][0]["name_enus"]
+
+    return item_name
