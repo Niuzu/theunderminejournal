aa4e141869b5234e40809c5fc5cce7461076b7ab
Niuzu <markusherzberg94@gmail.com>
2018-09-25 05:41:54 +0200

    added get_item_current_price and get_item_quantity

--- getjson.py
+++ getjson.py
@@ -44,7 +44,54 @@ def get_item_name(item_id, realm_index) -> str:
         None
     """
 
-    #  get item ison and direct to name
-    item_name = __get_item_json__(item_id, realm_index)["stats"][0]["name_enus"]
+    #  get item json and direct to name
+    item_name = __get_item_json__(item_id, realm_index)[
+        "stats"][0]["name_enus"]
 
     return item_name
+
+
+def get_item_current_price(item_id, realm_index) -> int:
+    """
+    Returns current price from underminejournal json
+
+    Args:
+        item_id (int): Defines the item with an id. Id's can be found at: 'https://www.wowhead.com/items'
+        realm_index (int): Defines the realm with an id. List of Id's couln't be found. Thrall-EU = 238 
+
+    Returns:
+        int: Returns an integer in wow bronce currency of the current lowest price
+        None: Returns None if no items or data was found
+
+    Raises:
+        None
+    """
+
+    #  get item json and direct to name
+    item_price = __get_item_json__(item_id, realm_index)[
+        "stats"][0]["price"]
+
+    return item_price
+
+
+def get_item_quantity(item_id, realm_index) -> int:
+    """
+    Returns current quantity from underminejournal json
+
+    Args:
+        item_id (int): Defines the item with an id. Id's can be found at: 'https://www.wowhead.com/items'
+        realm_index (int): Defines the realm with an id. List of Id's couln't be found. Thrall-EU = 238 
+
+    Returns:
+        int: Returns an integer with the quantity avalable in the ah currently
+        None: Returns None if no items or data was found
+
+    Raises:
+        None
+    """
+
+    #  get item json and direct to name
+    item_quantity = __get_item_json__(item_id, realm_index)[
+        "stats"][0]["quantity"]
+
+    return item_quantity
